4279cb3d112363fa4687b734e0406c8a01db114b
Joao <j.bartapele@gmail.com>
2021-09-15 03:31:30 -0300

    - Layout inicial home.

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -6,6 +6,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatListModule } from '@angular/material/list';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [],
@@ -17,6 +18,7 @@ import { MatListModule } from '@angular/material/list';
     MatButtonModule,
     MatExpansionModule,
     MatListModule,
+    MatInputModule,
   ],
 })
 export class MaterialModule {}
